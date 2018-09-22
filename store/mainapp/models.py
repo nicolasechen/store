269a4163eaa6d15c2caf3e01f12e20dde541ab6c
Nico.Chen <nico@mail.eagleskytech.com>
2018-09-22 17:58:13 +0800

    update

--- store/mainapp/models.py
+++ store/mainapp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField('Category', max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    category = models.ForeignKey(Category)
+    name = models.CharFieldield('Name', max_length=20, unique=True)
+    price = models.IntegerField('Price', default=0)
+
+    def __str__(self):
+        return self.name
+
+class Inventory(models.Model):
+    item = models.ForeignKey(Item)
+
+    def __str__(self):
+            return self.name
+class Buying(models.Model):
+    date = models.DateField(auto_now_add=True)
+
+
+    def __str__(self):
+        return
--- store/mainapp/views.py
+++ store/mainapp/views.py
@@ -1,7 +1,9 @@
 # mainapp/views.py
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
 def get_index(request):
+
 
     return render(request, 'index.html',locals())
